27a460ee624ded4a2b4667be623995da81347199
Dale Sinder <sinder@illinois.edu>
2020-02-11 17:42:08 -0600

    Add and remove people from access list (assumes arcID 0 for now)

--- Notes2021Blazor/Server/Controllers/AccessListController.cs
+++ Notes2021Blazor/Server/Controllers/AccessListController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Notes2021Blazor.Shared;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Notes2021Blazor.Server.Controllers
@@ -15,11 +17,12 @@ namespace Notes2021Blazor.Server.Controllers
     public class AccessListController : ControllerBase
     {
         private readonly NotesDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public AccessListController(NotesDbContext db)
+        public AccessListController(NotesDbContext db, UserManager<IdentityUser> userManager)
         {
             _db = db;
-
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -51,6 +54,53 @@ namespace Notes2021Blazor.Server.Controllers
 
             _db.Update(work);
             await _db.SaveChangesAsync();
+        }
+
+        [HttpPost]
+        public async Task Post(NoteAccess item)
+        {
+            NoteAccess work = await _db.NoteAccess.Where(p => p.NoteFileId == item.NoteFileId
+                && p.ArchiveId == item.ArchiveId && p.UserID == item.UserID)
+                .FirstOrDefaultAsync();
+            if (work != null)
+                return;     // already exists
+
+            if (item.UserID == Globals.AccessOtherId())
+                return;     // can not create "Other"
+
+            _db.NoteAccess.Add(item);
+            await _db.SaveChangesAsync();
+        }
+
+        [HttpDelete]
+        public async Task Delete(string fileId)
+        {
+            string[] parts = fileId.Split(".");
+            if (parts.Length != 3)
+                return;
+
+            string uid = parts[2];
+            int fid = int.Parse(parts[0]);
+            int aid = int.Parse(parts[1]);
+
+            if (uid == Globals.AccessOtherId())
+                return;     // can not delete "Other"
+
+            // also can not delete self
+            string userName = this.HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+            IdentityUser user = await _userManager.FindByNameAsync(userName);
+            if (uid == user.Id)
+                return;     // can not delete self"
+
+            NoteAccess work = await _db.NoteAccess.Where(p => p.NoteFileId == fid
+                && p.ArchiveId == aid && p.UserID == uid)
+                .FirstOrDefaultAsync();
+            if (work == null)
+                return;
+
+            _db.NoteAccess.Remove(work);
+            await _db.SaveChangesAsync();
+
         }
 
     }
