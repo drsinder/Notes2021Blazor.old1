ec4b29f4693d60a8502cd800b1c128bf45f31fc1
Dale Sinder <sinder@illinois.edu>
2020-02-14 14:55:06 -0600

    Speed up recent notes finder

---
+++ Notes2021Blazor/Server/Controllers/NoteIndexFileController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notes2021Blazor.Shared;
+
+namespace Notes2021Blazor.Server.Controllers
+{
+    [Authorize(Roles = "User")]
+    [Route("api/[controller]")]
+    [Route("api/[controller]/{sid}")]
+    [ApiController]
+    public class NoteIndexFileController : ControllerBase
+    {
+        private readonly NotesDbContext _db;
+
+        public NoteIndexFileController(NotesDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<NoteFile> Get(string sid)
+        {
+            int nfid = int.Parse(sid);
+            return _db.NoteFile.SingleOrDefault(p => p.Id == nfid);
+        }
+    }
+}
